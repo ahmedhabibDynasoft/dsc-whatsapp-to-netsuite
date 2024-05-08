24700d0eb57a7122a7937b4e40ab9fbf5d144edd
Muhammad Ahmed <ahmed.habib@dynasoftcloud.com>
2024-05-08 10:29:46 +0500

    remove get request api

--- routes/api.js
+++ routes/api.js
@@ -4,14 +4,8 @@ const fetch = require('node-fetch')
 
 router.get('/', async (req, res) => {
   try {
-    const response = await fetch('https://tstdrv2207690.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=4572&deploy=3&compid=TSTDRV2207690&h=a31756dad7fadab9ed25', {
-      method: 'GET',
-      params: req.params,
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
-      }
-    })
-    console.log('GET => ', response);
+    
+    console.log('GET => ', req.params);
 
     res.status(200).send(req.params['hub.challenge'])
   }
