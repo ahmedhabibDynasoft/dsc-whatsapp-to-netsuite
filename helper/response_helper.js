a90a91a0433c30d0af5e60fb5a2c457162a573d3
ahmed khan <ahmed.afridi091@gmail.com>
2024-05-10 11:46:57 +0500

    changes

--- helper/response_helper.js
+++ helper/response_helper.js
@@ -1,5 +1,7 @@
+let body={"object":"whatsapp_business_account","entry":[{"id":"110894171826159","changes":[{"value":{"messaging_product":"whatsapp","metadata":{"display_phone_number":"15550625257","phone_number_id":"104529472471761"},"statuses":[{"id":"wamid.HBgMOTIzMzEyMjA4ODg1FQIAERgSRjZGNzQ5QjEyOEZEOEY3NTBCAA==","status":"delivered","timestamp":"1715316288","recipient_id":"923312208885","conversation":{"id":"54e19e8239d83c4e1f797223ed0d2d58","origin":{"type":"marketing"}},"pricing":{"billable":true,"pricing_model":"CBP","category":"marketing"}}]},"field":"messages"}]}]}
 var responseHelper = data => {
     let resp = {
+        conversationId:"",
         from: "",
         message_id: "",
         timestamp: "",
@@ -11,18 +13,21 @@ var responseHelper = data => {
 
     if (!testData.statuses && !testData.messages[0].context) {
 
+        resp.conversationId = data.entry[0].id
         resp.message_id = testData.messages[0].id
         resp.message = testData.messages[0].text.body
         resp.type = testData.messages[0].type
         resp.timestamp = testData.messages[0].timestamp
         resp.from = testData.messages[0].from
     } else if (testData.statuses) {
+        resp.conversationId = data.entry[0].id
         resp.from = testData.statuses[0].recipient_id
         resp.message_id = testData.statuses[0].id
         resp.status = testData.statuses[0].status
         resp.type = "status"
         resp.timestamp = testData.statuses[0].timestamp
     } else {
+        resp.conversationId = data.entry[0].id
         resp.message_id = testData.messages[0].id
         resp.message = testData.messages[0].text.body
         resp.type = "Respond"
@@ -33,5 +38,5 @@ var responseHelper = data => {
 
     return resp
 }
-
+console.log(responseHelper(body));
 module.exports = { responseHelper }
